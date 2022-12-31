a01a9bfa79aa02580057d5f49c7fd0bbd3576a4a
Mayank Kashyap <107757811+Memomer@users.noreply.github.com>
2022-12-31 15:42:45 +0530

    Create endians.c

---
+++ Code/Endians/Endians.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+int main(){
+   uint16_t val = 0x400;
+   unint8_t* ptr = (uint8_t*)&val;
+
+   if (ptr[0] == 0x40){ 
+    printf("big endian\n");
+   }
+
+   else if (ptr[1] == 0x40) {
+    printf("little endian\n");
+   }
+
+   else{
+    printf("unknown endianness");
+   }
+}
